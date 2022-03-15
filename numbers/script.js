808c0e35466e0664972ea27293be71bcd0f59f3a
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-15 09:59:26 +0800

    Completes lessons on variables and numbers

--- numbers/script.js
+++ numbers/script.js
@@ -74,3 +74,59 @@ console.log(numberObject1 == numberObject2);    // false
 console.log(numberObject1 === numberObject2);    // false
 console.log(numberObject1 == numberObject3);    // true
 console.log(numberObject1 === numberObject3);    // false
+
+
+// Number Object Methods
+const lotsOfDecimals = 1.234567891023456789;
+console.log(lotsOfDecimals.toFixed(3));
+
+const userAgeString = "30";
+const userAgeInt = Number(userAgeString);
+console.log(typeof userAgeString, typeof userAgeInt);
+
+
+/*
+COMPARISON OPERATORS:
+    ===	    Strict equality	Tests whether the left and right values are identical to one another	                5 === 2 + 4
+    !==	    Strict-non-equality	Tests whether the left and right values are not identical to one another	        5 !== 2 + 3
+    <	    Less than	Tests whether the left value is smaller than the right one.	                                10 < 6
+    >	    Greater than	Tests whether the left value is greater than the right one.	                            10 > 20
+    <=	    Less than or equal to	Tests whether the left value is smaller than or equal to the right one.	        3 <= 2
+    >=	    Greater than or equal to	Tests whether the left value is greater than or equal to the right one.     5 >= 4
+
+
+Note: You may see some people using == and != in their tests for equality and non-equality. 
+These are valid operators in JavaScript, but they differ from ===/!==. 
+The former versions test whether the values are the same but not whether the values' datatypes are the same. 
+The latter, strict versions test the equality of both the values and their datatypes. 
+The strict versions tend to result in fewer errors, so we recommend you use them.
+*/
+
+
+// + can be used as a unary operator to covert other datatypes to numbers
+//  similar to Number() but shorter and cleaner
+console.log(+true);
+console.log(+'100');
+console.log(+'');
+
+
+let input1 = '1';
+let input2 = '2';
+console.log(input1 + input2);
+console.log(+input1 + +input2);
+
+
+// Weird Stuff with the Assignment Operator and more
+let num1 = 1, num2 = 2, num3 = 3;
+let result = num1 + (num2 *= num3);
+console.log(num1, num2, num3, result);
+
+a = b = c = 123;
+console.log(a, b, c);
+
+a = 2;
+a *= 3 + 5;
+console.log(a);     // returns 16 (i.e. 2 * (3 + 5)) because shorthand operators are lower in precedence
+
+a = (1 + 2, 3 + 4);
+console.log(a);
