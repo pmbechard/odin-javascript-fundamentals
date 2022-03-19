e23760094a888a6a9031c7c6e093defeb8849963
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-19 14:34:42 +0800

    Completes debugging lesson

--- debugging/script.js
+++ debugging/script.js
@@ -4,3 +4,12 @@ console.log('Before the line-of-code breakpoint');
 debugger;
 console.log('After the line-of-code breakpoint');
 
+function tester() {
+    console.log('This is a test for a function breakpoint:');
+    console.log('\tIf the debugger stopped before these logs, test was successful.');
+}
+
+// Function debugger not working. Cannot find solution...
+// debug(tester);
+// tester();
+
